a2a5741cc3c768a1122828c158bbe906bd6b7ab5
Lukas Nord <luke.nord@gmail.com>
2018-01-13 18:55:35 -0600

    cancel button setup to clear inputs and return to normal state.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,7 @@ $(document).ready(startUp);
 
 function startUp(){
     $('#createTask').on('click', createTask )
-
+    $('#createTaskCancel').on('click', createTaskCancelBtn);
 }
 
 
@@ -20,5 +20,12 @@ function startUp(){
     $('.middle-content').css('opacity',0.1);
     $('#create-task-dialogue').css('display','block');
 
+ }
+ function createTaskCancelBtn(){
+    $('#create-task-dialogue').find('input:text').val(''); 
+    $('#createTaskDescription').val('');
+    $('#createTaskDate').val('');
+    $('#create-task-dialogue').css('display','none');
+    $('.middle-content').css('opacity', 1);
 
  }
